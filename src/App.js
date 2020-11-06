3a060017c4fbbcc96f7c2e0df5bbf570c33fd1e9
benubach <benubach@gmail.com>
2020-11-06 13:58:08 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+import "./style.css";
+
+/*
+  Instructions:
+  1. Get a list of users by calling "getUsers" from "/src/api.js"
+  2. Render the list of users in the page
+*/
+
+export default function App() {
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
+  );
+}
